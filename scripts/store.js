274a457d1fc8e78669c3258ce7cc670b79ef04df
Selvin Norman <selvin715@gmail.com>
2018-07-23 15:15:38 -0700

    Update store to create, update, delete items

--- scripts/store.js
+++ scripts/store.js
@@ -7,13 +7,55 @@ const store = (function () {
         { id: cuid(), name: 'milk', checked: true },
         { id: cuid(), name: 'bread', checked: false }
       ];
-      let hideCheckedItems = false;
-      let searchTerm = '';
-    
+    let hideCheckedItems = false;
+    let searchTerm = '';
+    function findById(id) {
+        store.items.find(function(element){
+            if (element.id === id){
+                return element.id;
+            }
+        }
+    };
+    function addItem(name) {
+        try {
+            Item.validateName(name);
+            store.items.push(Item.create(itemName));
+        } catch(error) {
+            console.log(`Cannot add item: ${error.message}`);
+        }
+        render();
+        }
+    };
+    function findAndToggleChecked(id) {       
+        const foundItem = this.findById(id);
+        foundItem.checked = !foundItem.checked;
+    };
+
+    function findAndUpdateName(id, newName) {
+        try {
+            Item.validateName(newName);
+        } catch(error) {
+            console.log(`Cannot update name: ${error.message}`);
+        }
+        const validItem = this.findById(id);
+        console.log(`validItem: ${validItem}`);
+        validItem.name = newName;
+        render();
+        }        
+    };
+    function findAndDelete(id) {
+        const indexToDelete = this.items.findIndex(findById(id));
+        console.log(`this.items.findIndex(findById(id)): ${this.items.findIndex(findById(id))}`);
+        this.items.splice(indexToDelete);
+    }       
+       
     return {
-        items: items,
-        hideCheckedItems: hideCheckedItems, 
-        searchTerm: searchTerm,
+        items,
+        hideCheckedItems, 
+        searchTerm,
+        findById,
+        addItem,
+
     };         
  }() );
 
